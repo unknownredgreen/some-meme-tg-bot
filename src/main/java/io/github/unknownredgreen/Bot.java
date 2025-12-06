d21748601fe14f54eb5d51a982b5059eb9040092
unknownredgreen <200398024+unknownredgreen@users.noreply.github.com>
2025-12-06 20:52:15 +0200

    First working version

--- src/main/java/io/github/unknownredgreen/Bot.java
+++ src/main/java/io/github/unknownredgreen/Bot.java
@@ -1,5 +1,103 @@
 package io.github.unknownredgreen;
 
-class Bot {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.List;
+import java.util.Random;
+
+@Slf4j
+@RequiredArgsConstructor
+class Bot extends TelegramLongPollingBot {
+    private final String botUsername;
+    private final String botToken;
+    @Getter
+    private final List<String> data;
+    private final Random random;
+    private final int maxValue = 1000;
+
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (!update.hasMessage()) return;
+        Message msg = update.getMessage();
+        if (!msg.hasText()) return;
+        long chatId = msg.getChatId();
+        String text = msg.getText().replaceAll("\\R", " ");
+        updateData(text);
+        if (random.nextInt(0, 10) == 9) {
+            if (random.nextInt(0, 10) == 9) {
+                try {
+                    execute(SendMessage.builder()
+                            .chatId(String.valueOf(chatId))
+                            .text(getRandomGeneratedString())
+                            .replyToMessageId(msg.getMessageId())
+                            .build());
+                } catch (TelegramApiException e) {
+                    log.warn(e.getMessage());
+                }
+            } else {
+                try {
+                    execute(SendMessage.builder()
+                            .chatId(String.valueOf(chatId))
+                            .text(getRandomGeneratedString())
+                            .build());
+                } catch (TelegramApiException e) {
+                    log.warn(e.getMessage());
+                }
+            }
+        }
+    }
+
+    private void updateData(String str) {
+        if (str.length() > 100) return;
+        if (data.contains(str)) return;
+
+        if (random.nextInt(0, 10) == 9) {
+            str = str.replace(" ", "");
+        }
+
+        int size = data.size();
+
+        if (size < maxValue) {
+            data.add(str);
+        } else if (size == maxValue) {
+            data.set(random.nextInt(0, maxValue), str);
+        } else {
+            while (data.size() > maxValue) {
+                data.removeLast();
+            }
+        }
+    }
+
+    private String getRandomGeneratedString() {
+        String baseString1 = data.get(random.nextInt(0, data.size()));
+        String baseString2 = data.get(random.nextInt(0, data.size()));
+
+        StringBuilder sb = new StringBuilder();
+
+        if (baseString1.length() < baseString2.length()) {
+            sb.append(baseString1).append(" ").append(baseString2);
+        } else {
+            sb.append(baseString2).append(" ").append(baseString1);
+        }
+
+        return sb.toString();
+    }
 }
--- src/main/java/io/github/unknownredgreen/Main.java
+++ src/main/java/io/github/unknownredgreen/Main.java
@@ -1,5 +1,50 @@
 package io.github.unknownredgreen;
 
-public class Main {
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.IOException;
+import java.util.Random;
+
+public class Main {
+    private static FileManager fileManager;
+    private static final Random random = new Random();
+
+    public static void main(String[] args) throws TelegramApiException, IOException {
+        if (args.length < 3) {
+            throw new RuntimeException("""
+                    Too few args
+                    Required:
+                    1: bot username
+                    2: bot token
+                    3: data file path, like /home/mainuser/Desktop/telegram_bot/data.txt
+                    """);
+        } else if (args.length > 3) {
+            throw new RuntimeException("""
+                    Too many args
+                    Required:
+                    1: bot username
+                    2: bot token
+                    3: data file path, like /home/mainuser/Desktop/telegram_bot/data.txt
+                    """);
+        }
+
+        String botUsername = args[0];
+        String botToken = args[1];
+        String dataFilePath = args[2]; // like /home/mainuser/Desktop/telegram_bot/data.txt
+
+        fileManager = new FileManager(dataFilePath);
+
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        Bot bot = new Bot(botUsername, botToken, fileManager.load(), random);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                fileManager.save(bot.getData());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+        botsApi.registerBot(bot);
+    }
 }
