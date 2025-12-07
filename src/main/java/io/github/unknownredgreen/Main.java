86a32dc1cc7fa700c21d80bc0d8e51eef1db265f
unknownredgreen <200398024+unknownredgreen@users.noreply.github.com>
2025-12-07 12:51:44 +0200

    Add config file argument and config init

--- src/main/java/io/github/unknownredgreen/Main.java
+++ src/main/java/io/github/unknownredgreen/Main.java
@@ -1,5 +1,7 @@
 package io.github.unknownredgreen;
 
+import io.github.unknownredgreen.files.ConfigFileManager;
+import io.github.unknownredgreen.files.SavedDataFileManager;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -8,39 +10,48 @@ import java.io.IOException;
 import java.util.Random;
 
 public class Main {
-    private static FileManager fileManager;
+    private static SavedDataFileManager savedDataFileManager;
+    private static ConfigFileManager configFileManager;
     private static final Random random = new Random();
 
     public static void main(String[] args) throws TelegramApiException, IOException {
-        if (args.length < 3) {
+        int neededArgCount = 4;
+        String requiredArgsAdvice = """
+                Required:
+                1: bot username
+                2: bot token
+                3: data file path, like /home/mainuser/Desktop/telegram_bot/data.txt
+                4: config file path, like /home/mainuser/Desktop/telegram_bot/config.txt
+                """;
+
+        if (args.length < neededArgCount) {
             throw new RuntimeException("""
                     Too few args
-                    Required:
-                    1: bot username
-                    2: bot token
-                    3: data file path, like /home/mainuser/Desktop/telegram_bot/data.txt
-                    """);
-        } else if (args.length > 3) {
+                    %s
+                    """.formatted(requiredArgsAdvice));
+        } else if (args.length > neededArgCount) {
             throw new RuntimeException("""
                     Too many args
-                    Required:
-                    1: bot username
-                    2: bot token
-                    3: data file path, like /home/mainuser/Desktop/telegram_bot/data.txt
-                    """);
+                    %s
+                    """.formatted(requiredArgsAdvice));
         }
 
         String botUsername = args[0];
         String botToken = args[1];
         String dataFilePath = args[2]; // like /home/mainuser/Desktop/telegram_bot/data.txt
+        String configFilePath = args[3]; //like /home/mainuser/Desktop/telegram_bot/config.txt
 
-        fileManager = new FileManager(dataFilePath);
+        savedDataFileManager = new SavedDataFileManager(dataFilePath);
+        configFileManager = new ConfigFileManager(configFilePath);
+        configFileManager.init();
 
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        Bot bot = new Bot(botUsername, botToken, fileManager.load(), random);
+        Bot bot = new Bot(botUsername, botToken, savedDataFileManager.load(), configFileManager, random);
+
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try {
-                fileManager.save(bot.getData());
+                savedDataFileManager.save(bot.getData());
+                configFileManager.fixSelf();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
