d933d024311b4827a65e5c37d59e51c4ddefaba2
unknownredgreen <200398024+unknownredgreen@users.noreply.github.com>
2025-12-10 18:48:49 +0200

    Now all configs are in one place: ConfigStorage

---
+++ src/main/java/io/github/unknownredgreen/ConfigStorage.java
@@ -0,0 +1,60 @@
+package io.github.unknownredgreen;
+
+import io.github.unknownredgreen.files.ConfigFileManager;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+public final class ConfigStorage {
+    private final ConfigFileManager configFileManager;
+    private String[] stickerIds;
+    private String[] reactionEmojis;
+    private int maxDataLength;
+    private boolean sendingStickers = true;
+    private boolean reactingToMessages = true;
+
+    public ConfigStorage(ConfigFileManager configFileManager) {
+        this.configFileManager = configFileManager;
+
+        try {
+            maxDataLength = configFileManager.parseInt("maxDataLength");
+            //i don`t think you even need < 5 data saved
+            if (maxDataLength < 5) {
+                throw new RuntimeException("Max data length can`t be < 5. Change maxDataLength in %s".formatted(configFileManager.getConfigFilePath()));
+            }
+        } catch (NumberFormatException | NullPointerException e) {
+            log.debug("No maxDataLength found in config. Using default: 1000");
+            maxDataLength = 1000;
+        }
+
+        try {
+            stickerIds = configFileManager.parseStringArray("stickerIds");
+        } catch (NullPointerException e) {
+            log.debug("No stickerIds found in config. Using default: no sticker sending");
+            sendingStickers = false;
+        }
+
+        try {
+            reactionEmojis = configFileManager.parseStringArray("reactionEmojis");
+        } catch (NullPointerException e) {
+            log.debug("No reactionEmojis found in config. Using default: no reacting to messages");
+            reactingToMessages = false;
+        }
+
+
+        try {
+            sendingStickers = configFileManager.parseBoolean("sendingStickers");
+        } catch (NullPointerException e) {}
+        if (stickerIds == null || stickerIds.length == 0) {
+            sendingStickers = false;
+        }
+
+        try {
+            reactingToMessages = configFileManager.parseBoolean("reactingToMessages");
+        } catch (NullPointerException e) {}
+        if (reactionEmojis == null || reactionEmojis.length == 0) {
+            reactingToMessages = false;
+        }
+    }
+}
