a509d02bd7c864982c494c1038fd8ae2ddd1325e
unknownredgreen <200398024+unknownredgreen@users.noreply.github.com>
2026-04-09 09:16:38 +0300

    Add --create option to auto-create not found files specified in args, add logging about creating and checking files (MINOR++: 1.2.0 now)

--- src/main/java/io/github/unknownredgreen/Main.java
+++ src/main/java/io/github/unknownredgreen/Main.java
@@ -9,6 +9,9 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Random;
 
@@ -21,12 +24,15 @@ public class Main {
 
     public static void main(String[] args) throws TelegramApiException, IOException {
         int neededArgCount = 4;
+        int maxOptionalArgCount = 1;
         String requiredArgsAdvice = """
                 Required:
                 1: bot username
                 2: bot token
                 3: data file path, like /home/mainuser/Desktop/telegram_bot/data.txt
                 4: config file path, like /home/mainuser/Desktop/telegram_bot/config.txt
+                Optional:
+                5: -c | --create (to create the directories and the files specified in file path arguments if not found)
                 """;
 
         if (args.length < neededArgCount) {
@@ -34,7 +40,7 @@ public class Main {
                     Too few args
                     %s
                     """.formatted(requiredArgsAdvice));
-        } else if (args.length > neededArgCount) {
+        } else if (args.length > neededArgCount + maxOptionalArgCount) {
             throw new RuntimeException("""
                     Too many args
                     %s
@@ -45,6 +51,18 @@ public class Main {
         String botToken = args[1];
         String dataFilePath = args[2]; // like /home/mainuser/Desktop/telegram_bot/data.txt
         String configFilePath = args[3]; //like /home/mainuser/Desktop/telegram_bot/config.txt
+
+        boolean createNotFoundFiles = false;
+
+        for (int i = 4; i < args.length; i++) {
+            switch (args[i]) {
+                case "-c", "--create": createNotFoundFiles = true; break;
+
+                default: throw new IllegalArgumentException("No such option '%s'".formatted(args[i]));
+            }
+        }
+
+        ensureFilesExist(createNotFoundFiles, dataFilePath, configFilePath);
 
         savedDataFileManager = new SavedDataFileManager(dataFilePath);
         configFileManager = new ConfigFileManager(configFilePath);
@@ -71,5 +89,36 @@ public class Main {
             }
         }));
         botsApi.registerBot(bot);
+    }
+
+    private static void ensureFilesExist(boolean createNotFoundFiles, String... filePathsStrs) throws IOException {
+        for (String filePathStr : filePathsStrs) {
+            Path path;
+            try {
+                path = Path.of(filePathStr);
+            } catch (InvalidPathException e) {
+                throw new RuntimeException("Path %s is invalid".formatted(filePathStr));
+            }
+
+            if (Files.exists(path)) {
+                if (Files.isDirectory(path)) {
+                    throw new RuntimeException("%s is a directory and not a file".formatted(filePathStr));
+                }
+            } else {
+                if (createNotFoundFiles) {
+                    Path parentPath = path.getParent();
+                    if (parentPath != null && !Files.exists(parentPath)) {
+                        Files.createDirectories(parentPath);
+                        log.info("Created needeed directories {}", parentPath);
+                    }
+                    Files.createFile(path);
+                    log.info("Created file {}", path);
+                } else {
+                    throw new RuntimeException("File at path %s does not exist (use --create to create files that weren`t found)"
+                            .formatted(filePathStr)
+                    );
+                }
+            }
+        }
     }
 }
